f9203dbd3741714f5314e1daaf4ef968805433c0
Bryan Silva <bryanjsilva04@gmail.com>
2023-05-30 13:07:42 -0700

    - Added PWA

--- zotmeal-vite/vite.config.ts
+++ zotmeal-vite/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          injectRegister: 'auto',
+          devOptions: {
+              enabled: true
+        }
+      })
+  ],
   resolve: {
     alias: {
       'react-native': 'react-native-web',
