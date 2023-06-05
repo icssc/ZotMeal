17fc1e8bbb18aa139778ce22e9b9ebc4fae9a2ed
Bryan Silva <bryanjsilva04@gmail.com>
2023-06-04 22:50:03 -0700

    Changed color?

--- zotmeal-vite/vite.config.ts
+++ zotmeal-vite/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
           manifest: {
               name: 'Zotmeal',
               description: 'zotmeal',
-              theme_color: '#ffffff',
+              theme_color: '#62B2FF',
               icons: [
                   {
                       src: 'imageAssets/FaviconSet/android-chrome-192x192.png',
