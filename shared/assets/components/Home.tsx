3fa251a48b03e31d2e3b94b107716970ed039950
Bryan Silva <thisperson101@outlook.com>
2023-07-22 22:47:50 -0700

    Fixed swipeable view bug - Added media query to disable swipeable view when widescreen - Added event listener to properly handle changing screen sizes when on second screen

--- shared/assets/components/Home.tsx
+++ shared/assets/components/Home.tsx
@@ -1,12 +1,29 @@
 import Location from "./Location";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Home.css";
 import SwipeableViews from "react-swipeable-views";
 
 
 function Home(){
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const [matches, setMatches] = useState(
+        window.matchMedia("all and (max-width:850px)").matches
+      )
+
+    useEffect(() => {
+       window.matchMedia("all and (max-width:850px)").addEventListener('change', e => {
+           setMatches( e.matches )
+           setSlideIndex(0)
+       });
+    }, []);
+
+    const handleSwitch = (index, type) => {
+        setSlideIndex(index)
+    }
+
     return (
-        <SwipeableViews enableMouseEvents className="home">
+        <SwipeableViews enableMouseEvents disabled={!matches} index={slideIndex} onChangeIndex={handleSwitch} className="home">
             <Location location = "brandywine"/>
             <Location location = "anteatery"/>
         </SwipeableViews>
