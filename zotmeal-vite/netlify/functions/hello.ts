fd65f2a7b1dc50adcd2f72f46aa942519a1b362e
Katy Huang <katyhuang360166@gmail.com>
2023-09-09 16:27:06 -0700

    fetch menu data with netlify serverless function and update error message

--- zotmeal-vite/netlify/functions/hello.ts
+++ zotmeal-vite/netlify/functions/hello.ts
@@ -1,6 +1,9 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
