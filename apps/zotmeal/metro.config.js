f05272d90e7f042e45a8ac928807eb2ebe172efa
Aponia <brianmv@uci.edu>
2023-11-29 14:42:22 -0800

    feat: mvp zotmeal app

---
+++ apps/zotmeal/metro.config.js
@@ -0,0 +1,26 @@
+const path = require("node:path");
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+module.exports = () => {
+  const config = getDefaultConfig(__dirname);
+
+  if (config.resolver) {
+    // 1. Watch all files within the monorepo
+    config.watchFolders = [workspaceRoot];
+
+    // 2. Let Metro know where to resolve packages and in what order
+    config.resolver.nodeModulesPaths = [
+      path.resolve(projectRoot, "node_modules"),
+      path.resolve(workspaceRoot, "node_modules"),
+    ];
+
+    // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+    // config.resolver.disableHierarchicalLookup = true;
+  }
+
+  return config;
+};
---
+++ apps/zotmeal/src/routes/+page.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+
+export function Home() {
+  return (
+    <View className="p-8 h-full w-full">
+      <View className="w-full max-w-2xl h-full mx-auto">
+      </View>
+    </View>
+  );
+}
