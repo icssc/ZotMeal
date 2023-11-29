1a808f5ac19b984cb0b71fbdcf8727983ddcd0e8
Aponia <brianmv@uci.edu>
2023-11-29 14:45:38 -0800

    fix: removed bad dependecies from project root

--- apps/zotmeal/src/routes/+page.tsx
+++ apps/zotmeal/src/routes/+page.tsx
@@ -1,9 +1,10 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export function Home() {
   return (
     <View className="p-8 h-full w-full">
       <View className="w-full max-w-2xl h-full mx-auto">
+        <Text className="text-2xl font-bold">ZotMeal</Text>
       </View>
     </View>
   );
