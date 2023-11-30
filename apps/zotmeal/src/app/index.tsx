e35359bb5408f52a96915b031e5520f24428f2be
Aponia <brianmv@uci.edu>
2023-11-29 16:12:42 -0800

    feat: mvp with nativewind

--- apps/zotmeal/src/app/index.tsx
+++ apps/zotmeal/src/app/index.tsx
@@ -2,9 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="p-8 h-full w-full">
-      <View className="w-full max-w-2xl h-full mx-auto">
-        <Text className="text-4xl font-bold">ZotMeal</Text>
+    <View className="h-full w-full max-w-2xl mx-auto p-4">
+      <View className="h-full w-full p-4 border-4 rounded-xl">
+        <Text className="text-6xl md:text-8xl text-center font-extrabold bg-clip-text  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">ZotMeal</Text>
       </View>
     </View>
   );
