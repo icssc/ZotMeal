5d5aba69ac5bd35a95f1b9f5dc772c1a491b4453
Aponia <brianmv@uci.edu>
2023-11-29 16:25:09 -0800

    chore: remove gradients that don't actually do anything

--- apps/zotmeal/src/app/index.tsx
+++ apps/zotmeal/src/app/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <View className="h-full w-full max-w-2xl mx-auto p-4">
       <View className="h-full w-full p-4 border-4 rounded-xl">
-        <Text className="text-6xl md:text-8xl text-center font-extrabold bg-clip-text  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">ZotMeal</Text>
+        <Text className="text-6xl md:text-8xl text-center font-extrabold">ZotMeal</Text>
       </View>
     </View>
   );
