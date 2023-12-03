070e7556b08cee8069d13965dba9916b584a3d4b
Aponia <brianmv@uci.edu>
2023-12-03 01:54:08 -0800

    feat: mvp

--- apps/zotmeal/src/stores/theme.ts
+++ apps/zotmeal/src/stores/theme.ts
@@ -8,20 +8,31 @@ export interface ThemeStore {
   colorScheme: ColorSchemeName;
   width: number;
   setColorScheme: typeof Appearance.setColorScheme
+  bgColor: string;
+  textColor: string;
 }
 
 export const useThemeStore = create<ThemeStore>((set) => {
   Appearance.addChangeListener((preferences) => {
     set({ colorScheme: preferences.colorScheme });
+    set({ bgColor: preferences.colorScheme === "dark" ? 'bg-black' : 'bg-white' });
+    set({ textColor: preferences.colorScheme === "dark" ? 'text-white' : 'text-black' });
   });
 
   Dimensions.addEventListener("change", ({ window }) => {
     set({ width: window.width });
   })
 
+  const colorScheme = Appearance.getColorScheme();
+
+  const bgColor = colorScheme === 'dark' ? 'bg-black' : 'bg-white';
+  const textColor = colorScheme === 'dark' ? 'text-white' : 'text-black';
+
   return {
-    colorScheme: Appearance.getColorScheme(),
+    colorScheme,
     setColorScheme: Appearance.setColorScheme,
     width: Dimensions.get("window").width,
+    bgColor,
+    textColor,
   };
 });
