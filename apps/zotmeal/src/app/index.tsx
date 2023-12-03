d6adcc03b223cf85a1a6961904bf4453924b7047
Aponia <brianmv@uci.edu>
2023-12-03 01:08:57 -0800

    feat: current data example

--- apps/zotmeal/src/app/index.tsx
+++ apps/zotmeal/src/app/index.tsx
@@ -1,13 +1,20 @@
 import { Text, View } from "react-native";
 import { useRestaurantStore } from "../stores/restaurant";
+import { data } from '../lib/data'
+import { useMemo } from "react";
 
 export default function Home() {
   const restaurant = useRestaurantStore(store => store.restaurant)
+
+  const currentData = useMemo(() => {
+    return data[restaurant]
+  }, [restaurant])
 
   return (
     <View className="h-full w-full max-w-2xl mx-auto p-4">
       <View className="h-full w-full p-4 border-4 rounded-xl">
         <Text className="text-2xl md:text-4xl text-center font-semibold text-red-400">{restaurant}</Text>
+        <Text className="text-white">{JSON.stringify(currentData, undefined, 2)}</Text>
       </View>
     </View>
   );
