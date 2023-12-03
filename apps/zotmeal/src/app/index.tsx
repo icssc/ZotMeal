2acbb40b979c4d88a8bbb844497f014308aa8175
Aponia <brianmv@uci.edu>
2023-12-03 02:36:16 -0800

    feat: icons in navbar

--- apps/zotmeal/src/app/index.tsx
+++ apps/zotmeal/src/app/index.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 import { useRestaurantStore } from "../stores/restaurant";
 import { data } from '../lib/data'
 import { useThemeStore } from "../stores/theme";
@@ -14,57 +14,59 @@ export default function Home() {
 
   return (
     <ScrollView>
-      <View className="h-full w-full max-w-2xl mx-auto">
-        <View className="h-full w-full border-4 rounded-xl">
-          <View className="gap-8">
-            {currentData.all.map((category) => {
-              return (
-                <View key={category.station} className="flex gap-4 rounded">
-                  <Text className={`text-2xl font-semibold ${textColor}`}>
-                    {category.station}
-                  </Text>
+      <View className="w-full h-full max-w-2xl mx-auto">
+        {currentData.all.map((category) => {
+          return (
+            <View key={category.station} className="rounded p-2">
+              <View className="flex-row items-center justify-between gap-4">
+                <Text className={`text-2xl font-semibold ${textColor} shrink`}>
+                  {category.station}
+                </Text>
 
-                  <ScrollView horizontal>
-                    {category.menu.map((menu, index) => {
-                      return (
-                        <View key={menu.category}>
-                          <View className={`rounded-xl overflow-hidden ${index > 0 && 'ml-3'} border border-white`}>
-                            <Text className={`text-xl font-semibold ${textColor} bg-blue-500 p-2`}>
-                              {menu.category} ({menu.items.length})
-                            </Text>
+                <Pressable className="px-4 py-2 bg-yellow-400 rounded-xl shrink-0">
+                  <Text className="">View All</Text>
+                </Pressable>
+              </View>
 
-                            {/* TODO: create separate components for menu items. Then add a max height and `expand` button */}
-                            <View className="flex gap-2 p-2">
-                              <View className="flex-row justify-between gap-3">
-                                <Text className={`${textColor} text-lg font-semibold`}>Dish</Text>
-                                <Text className={`${textColor} text-lg font-semibold`}>Calories</Text>
+              <ScrollView horizontal className="my-2">
+                {category.menu.map((menu, index) => {
+                  return (
+                    <View key={menu.category}>
+                      <View className={`rounded-xl overflow-hidden ${index > 0 && 'ml-3'} border border-white`}>
+                        <Text className={`text-xl font-semibold ${textColor} bg-blue-500 p-2`}>
+                          {menu.category} ({menu.items.length})
+                        </Text>
+
+                        {/* TODO: create separate components for menu items. Then add an `expand` button */}
+                        <View className="flex gap-2 p-2 max-h-[250px]">
+                          <View className="flex-row justify-between">
+                            <Text className={`${textColor} text-lg font-semibold`}>Dish</Text>
+                            <Text className={`${textColor} text-lg font-semibold ml-3`}>Calories</Text>
+                          </View>
+
+                          <View className="w-full h-0 border border-white"></View>
+
+                          {menu.items.map((item) => {
+                            return (
+                              <View key={item.name} className="flex-row justify-between">
+                                <Text className={`${textColor}`}>{item.name}</Text>
+                                <Text className={`${textColor} ml-3`}>{item.nutrition.calories}</Text>
                               </View>
-
-                              <View className="w-full h-0 border border-white"></View>
-
-                              {menu.items.map((item) => {
-                                return (
-                                  <View key={item.name} className="flex-row justify-between gap-3">
-                                    <Text className={`${textColor}`}>{item.name}</Text>
-                                    <Text className={`${textColor}`}>{item.nutrition.calories}</Text>
-                                  </View>
-                                )
-                              })}
-                            </View>
-                          </View>
-
-                          <View className="hidden text-white">
-                            <Text>This takes up the remaining flex height.</Text>
-                          </View>
+                            )
+                          })}
                         </View>
-                      )
-                    })}
-                  </ScrollView>
-                </View>
-              )
-            })}
-          </View>
-        </View>
+                      </View>
+
+                      <View className="hidden text-white">
+                        <Text>This takes up the remaining flex height.</Text>
+                      </View>
+                    </View>
+                  )
+                })}
+              </ScrollView>
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
   );
--- apps/zotmeal/src/components/header/navbar.tsx
+++ apps/zotmeal/src/components/header/navbar.tsx
@@ -1,37 +1,30 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { useThemeStore } from "../../stores/theme";
-import { useMemo } from "react";
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export function Navbar() {
-  const colorScheme = useThemeStore((store) => store.colorScheme);
-
-  const bg = useMemo(() => {
-    switch (colorScheme) {
-      case "dark":
-        return "bg-black";
-      case "light":
-        return "bg-white";
-      default:
-        return "bg-black";
-    }
-  }, [colorScheme]);
-
-  const textColor = useMemo(() => {
-    switch (colorScheme) {
-      case "dark":
-        return "text-white";
-      case "light":
-        return "text-black";
-      default:
-        return "text-white";
-    }
-  }, [colorScheme]);
+  const bg = useThemeStore((store) => store.bgColor);
+  const textColor = useThemeStore((store) => store.textColor);
 
   return (
-    <View className={`${bg} h-12 justify-center`}>
+    <View className={`p-2 ${bg} h-12 flex-row justify-between items-center`}>
+      <Pressable className="flex-row items-center gap-2">
+        <Ionicons name="refresh-circle" size={24} color="#1284FD" />
+        <Text className={`${textColor}`}>Refresh</Text>
+      </Pressable>
+
       <Text className={`${textColor} text-2xl text-center font-extrabold`}>
         ZotMeal
       </Text>
+
+      <View className="flex-row items-center gap-2">
+        <Pressable>
+          <MaterialCommunityIcons name="heart-circle" size={24} color="#FE375F" />
+        </Pressable>
+        <Pressable>
+          <Ionicons name="ellipsis-horizontal-circle-sharp" size={24} color="#1284FD" />
+        </Pressable>
+      </View>
     </View>
   );
 }
