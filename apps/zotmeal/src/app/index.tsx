9c2efd3ea3821657e810810b5bc7a9c37e9ff0b6
Dennis Lustre <dennis.lustre@gmail.com>
2024-01-11 04:25:12 -0800

    feat: add mvp 'view all' button functionality

--- apps/zotmeal/src/app/index.tsx
+++ apps/zotmeal/src/app/index.tsx
@@ -1,42 +1,22 @@
 import { useMemo } from "react";
-import { Pressable, ScrollView, Text, View } from "react-native";
-import { useRestaurantStore } from "../stores/restaurant";
+import { ScrollView, View } from "react-native";
+import type { LocationInfo } from '../../../../packages/shared/lib/zotmeal.types';
+import Menus from "../components/Menus";
 import { data } from '../lib/data';
-import { useThemeStore } from "../stores/theme";
-import { Menu } from "./Menu";
+import { useRestaurantStore } from "../stores/restaurant";
 
 export default function Home() {
-  const restaurant = useRestaurantStore(store => store.restaurant)
-  const textColor = useThemeStore(store => store.textColor)
-
-  const currentData = useMemo(() => {
+  const restaurant = useRestaurantStore(store => store.restaurant);
+  const currentData: LocationInfo = useMemo(() => {
     return data[restaurant]
   }, [restaurant])
 
   return (
     <ScrollView>
       <View className="w-full h-full max-w-2xl mx-auto">
-        {currentData.all.map((category) => {
+        {currentData.all.map((station) => {
           return (
-            <View key={category.station} className="p-2 rounded">
-              <View className="flex-row items-center justify-between gap-4">
-                <Text className={`text-2xl font-semibold ${textColor} shrink`}>
-                  {category.station}
-                </Text>
-
-                <Pressable className="px-4 py-2 bg-yellow-400 rounded-xl shrink-0">
-                  <Text className="">View All</Text>
-                </Pressable>
-              </View>
-
-              <ScrollView horizontal className="my-2">
-                {category.menu.map((menu, index) => {
-                  return (
-                    <Menu menu={menu} key={menu.category} index={index} textColor={textColor} />
-                  )
-                })}
-              </ScrollView>
-            </View>
+            <Menus key={station.station} station={station} />
           )
         })}
       </View>
