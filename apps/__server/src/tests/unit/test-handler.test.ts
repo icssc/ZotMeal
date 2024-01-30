f0d58ea44e1b2b2d76d39faa627a26d04f82666e
Andrew Ho <andyminhtuanho@gmail.com>
2024-01-30 15:16:47 -0800

    Fixed linting problems

--- apps/__server/src/tests/unit/test-handler.test.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../app';
-import { expect, describe, it } from '@jest/globals';
-
-describe('Unit test for app handler', function () {
-});
