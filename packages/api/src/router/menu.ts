7f55f7f402896170d30a978574ef91a06c1d213b
Andrew Ho <andyminhtuanho@gmail.com>
2024-01-30 02:36:22 -0800

    oops extension

--- packages/api/src/router/menu.ts
+++ packages/api/src/router/menu.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const menuRouter = createTRPCRouter({
-  hello: publicProcedure.query(({ ctx }) => {
+  hello: publicProcedure.query(async ({ ctx }) => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(res.data);
     console.log("hello");
     const _ = ctx;
     return "hello";
