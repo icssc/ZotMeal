6926e0beed5abd8b4330c80997f43bd31f487375
Andrew Ho <andyminhtuanho@gmail.com>
2024-02-03 11:02:10 -0800

    updated schema

--- packages/api/src/router/menu.ts
+++ packages/api/src/router/menu.ts
@@ -3,6 +3,13 @@ import axios from "axios";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const menuRouter = createTRPCRouter({
+  get: publicProcedure.query(({ ctx }) => {
+    const _ = ctx;
+    // get a menu
+
+    // get it from prisma
+    // if not there, call parse procedure
+  }),
   hello: publicProcedure.query(async ({ ctx }) => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
     console.log(res.data);
