69fec3558d1fa2dca3dd9fc77bf1420da9a01682
Andrew Ho <andyminhtuanho@gmail.com>
2024-02-04 12:57:22 -0800

    feat: menu.get procedure  - get menu from db

--- packages/api/src/router/menu.ts
+++ packages/api/src/router/menu.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
+import { parse } from "date-fns";
+import { z } from "zod";
+
+import { MenuPeriod } from "@acme/db";
+import { parse as parseCampusDish } from "@zotmeal/utils";
+import {
+  CampusDishResponseSchema,
+  ParsedResponseSchema,
+} from "@acme/validators";
+
 import { createTRPCRouter, publicProcedure } from "../trpc";
-import { MenuPeriod } from '@acme/db';
-import {z} from "zod";
 
 // https://uci.campusdish.com/api/menu/GetMenus?locationId=3314&periodId=49&date=1/19/2024
 
@@ -14,64 +22,53 @@ import {z} from "zod";
 
 // const locations = ["a", "b", "c"] as const;
 
-
 const GetMenuSchema = z.object({
-  date: z.string().regex(RegExp("/^\d{2}\/\d{2}\/\d{4}$/")),
+  date: z.string().regex(RegExp("/^d{2}/d{2}/d{4}$/")),
   period: z.nativeEnum(MenuPeriod),
-  restaurant: z.string()
-})
+  restaurant: z.string(),
+});
 
-
-const getMenuProcedure = publicProcedure 
+const getMenuProcedure = publicProcedure
   .input(GetMenuSchema)
-  .query(({ ctx, input }) => {
+  .query(async ({ ctx, input }) => {
     const _ = ctx;
+
     // get a menu
-
-    const {date, period, restaurant} = input;
-
-    const {db} = ctx;
-
-    // date
-
-    const restaurantModel = db.restaurant.findFirst({
+    const { date: dateString, period, restaurant: restaurantName } = input;
+    const { db } = ctx;
+    const restaurant = await db.restaurant.findFirst({
       where: {
-        name: restaurant
+        name: restaurantName,
       },
       include: {
         stations: false,
         menu: false,
-      }
+      },
     });
-    if (restaurantModel === null) {
-      return 
-      
+    if (restaurant === null) {
+      return null;
     }
 
+    // 404 when the menu is not found
+    // we either deleted it or its not yet fetched
+    // do not allow the option to the client
 
-
+    const date = parse(dateString, "MM/dd/yyyy", new Date());
     // get the rstaurant with this name
 
+    const menu = await db.menu.findFirst({
+      where: {
+        restaurantId: restaurant.id,
+        date: date,
+        period: period,
+      },
+    });
 
-
-    db.menu.findUnique({where: {
-      date: date,
-      period: period,
-      restaurant: location
-    }})
-
-    // get it from prisma
-    // if not there, call parse procedure
-})
-
-
-
-
-import { parse } from "../../../utils/parse";
-import { CampusDishResponseSchema, ParsedResponseSchema } from "@acme/validators";
+    return menu;
+  });
 
 export const menuRouter = createTRPCRouter({
-  get: ,
+  get: getMenuProcedure,
   hello: publicProcedure.query(async ({ ctx }) => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
     console.log(res.data);
@@ -80,9 +77,11 @@ export const menuRouter = createTRPCRouter({
     return "hello";
   }),
   parse: publicProcedure.query(async ({ ctx }) => {
-    const res = await axios.get("https://uci.campusdish.com/api/menu/GetMenus?locationId=3314&periodId=49&date=1/19/2024");
+    const res = await axios.get(
+      "https://uci.campusdish.com/api/menu/GetMenus?locationId=3314&periodId=49&date=1/19/2024",
+    );
     const validated = CampusDishResponseSchema.parse(res.data);
-    const parsed = ParsedResponseSchema.parse(parse(validated));
+    const parsed = ParsedResponseSchema.parse(parseCampusDish(validated));
     const _ = ctx;
     return parsed;
   }),
