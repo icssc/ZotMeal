765cb73b540ea30f0ceb4a9cedf10e1d302977d0
Hamin Park <hmnpark0@gmail.com>
2024-02-07 10:37:08 -0800

    rename file

--- packages/api/src/router/restaurant/index.ts
+++ packages/api/src/router/restaurant/index.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "../../trpc";
-import { getScheduleProcedure } from "./get";
+import { getScheduleProcedure } from "./getSchedule";
 
 export const restaurantRouter = createTRPCRouter({
   getSchedule: getScheduleProcedure,
