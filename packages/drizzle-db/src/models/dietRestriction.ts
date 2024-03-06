3139101733954c1f783010067dbd0619f5faac68
Katy Huang <katyhuang360166@gmail.com>
2024-03-05 20:59:56 -0800

    refactor(schema): Split definitions and relations into separate modules for clarity and maintainability

---
+++ packages/drizzle-db/src/models/dietRestriction.ts
@@ -0,0 +1,29 @@
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+import { dish } from "./dish";
+
+export const dietRestriction = pgTable("DietRestriction", {
+  dishId: text("dishId")
+    .primaryKey()
+    .notNull()
+    .references(() => dish.id, { onDelete: "restrict", onUpdate: "cascade" }),
+  createdAt: timestamp("createdAt", { precision: 3, mode: "string" })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updatedAt", { precision: 3, mode: "string" }).notNull(),
+  containsFish: boolean("containsFish"),
+  containsMilk: boolean("containsMilk"),
+  containsPeanuts: boolean("containsPeanuts"),
+  containsSesame: boolean("containsSesame"),
+  containsShellfish: boolean("containsShellfish"),
+  containSoy: boolean("containSoy"),
+  containsTreeNuts: boolean("containsTreeNuts"),
+  containsWheat: boolean("containsWheat"),
+  isGlutenFree: boolean("isGlutenFree"),
+  isHalal: boolean("isHalal"),
+  isKosher: boolean("isKosher"),
+  isLocallyGrown: boolean("isLocallyGrown"),
+  isOrganic: boolean("isOrganic"),
+  isVegan: boolean("isVegan"),
+  isVegetarian: boolean("isVegetarian"),
+});
