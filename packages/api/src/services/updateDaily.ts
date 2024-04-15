fc9af599d910f10be658453cd8122104a137be72
Katy Huang <katyhuang360166@gmail.com>
2024-04-14 23:39:32 -0700

    fix: campusDishParams type

--- packages/api/src/services/updateDaily.ts
+++ packages/api/src/services/updateDaily.ts
@@ -1,27 +1,32 @@
 import { z } from "zod";
 
 import type { Drizzle } from "@zotmeal/db";
-import { MenuPeriodSchema, RestaurantSchema } from "@zotmeal/db/src/schema";
+import {
+  MenuPeriod,
+  MenuPeriodSchema,
+  RestaurantSchema,
+} from "@zotmeal/db/src/schema";
 import { DateRegex } from "@zotmeal/validators";
 
 import { getCampusDish, parseCampusDish } from "..";
+import { GetMenuParams } from "../menus/services/parse";
 
 export const UpdateDailySchema = z.object({
   date: DateRegex,
-  restaurantName: RestaurantSchema.shape.name
+  restaurantName: RestaurantSchema.shape.name,
 });
 export type UpdateDailyParams = z.infer<typeof UpdateDailySchema>;
 
 export async function updateDaily(
   db: Drizzle,
-  params: UpdateDailyParams
+  params: UpdateDailyParams,
 ): Promise<void> {
   for (const period in MenuPeriodSchema.shape.name.Enum) {
     const campusDishParams = {
       date: params.date,
-      period: period,
-      restaurant: params.restaurantName
-    }
+      period: period as MenuPeriod["name"],
+      restaurant: params.restaurantName,
+    } satisfies GetMenuParams;
 
     const campusDishResponse = await getCampusDish(campusDishParams);
     if (!campusDishResponse) {
