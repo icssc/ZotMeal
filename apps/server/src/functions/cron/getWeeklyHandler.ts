e02d4003206a2a1d1a84cc333bf70248bf26f6a6
Katy Huang <katyhuang360166@gmail.com>
2024-04-15 21:13:32 -0700

    chore: add script + add type + some bug fix

--- apps/server/src/functions/cron/getWeeklyHandler.ts
+++ apps/server/src/functions/cron/getWeeklyHandler.ts
@@ -1,23 +1,28 @@
 import { format } from "date-fns";
 
-import { getWeekInfo } from "@zotmeal/api/src/services/getWeekInfo";
+import {
+  getWeekInfo,
+  GetWeekInfoParams,
+} from "@zotmeal/api/src/services/getWeekInfo";
+import { createDrizzle } from "@zotmeal/db";
+import { Restaurant } from "@zotmeal/db/src/schema";
+
 import { RESTAURANT_NAMES } from "../../../../../packages/utils/src/constants";
-import { db } from "@zotmeal/db"
 
 export const main = async (event, context) => {
   try {
+    const db = await createDrizzle(process.env.DATABASE_URL);
+
     const now = new Date();
     const formattedTime = format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx");
     console.log(`Weekly task executed at: ${formattedTime}`);
 
     const formattedDate = format(now, "MM/dd/yyyy");
     for (const restaurant of RESTAURANT_NAMES) {
-      await getWeekInfo(db, 
-        {
-          date: formattedDate,
-          restaurantName: restaurant
-        }
-      );
+      await getWeekInfo(db, {
+        date: formattedDate,
+        restaurantName: restaurant as Restaurant["name"],
+      } satisfies GetWeekInfoParams);
     }
   } catch (error) {
     console.error("Failed to execute weekly task", error);
--- apps/server/src/functions/cron/updateDailyHandler.ts
+++ apps/server/src/functions/cron/updateDailyHandler.ts
@@ -1,24 +1,28 @@
 import { format } from "date-fns";
 
-import { updateDaily } from "@zotmeal/api/src/services/updateDaily";
+import {
+  updateDaily,
+  UpdateDailyParams,
+} from "@zotmeal/api/src/services/updateDaily";
+import { createDrizzle } from "@zotmeal/db";
+import { Restaurant } from "@zotmeal/db/src/schema";
+
 import { RESTAURANT_NAMES } from "../../../../../packages/utils/src/constants";
-import { db } from "@zotmeal/db"
 
 export const main = async (event, context) => {
   try {
+    const db = await createDrizzle(process.env.DATABASE_URL);
     const now = new Date();
     const formattedTime = format(now, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx");
-    console.log(`Weekly task executed at: ${formattedTime}`);
+    console.log(`Daily task executed at: ${formattedTime}`);
 
     const formattedDate = format(now, "MM/dd/yyyy");
 
     for (const restaurant of RESTAURANT_NAMES) {
-      await updateDaily(db, 
-        {
-          date: formattedDate,
-          restaurantName: restaurant
-        }
-      );
+      await updateDaily(db, {
+        date: formattedDate,
+        restaurantName: restaurant as Restaurant["name"],
+      } satisfies UpdateDailyParams);
     }
   } catch (error) {
     console.error("Failed to execute weekly task", error);
