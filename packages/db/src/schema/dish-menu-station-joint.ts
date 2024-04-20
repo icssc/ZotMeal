dd4d4c0796d3aad40ef73605d9927e4986430de0
Bryan Silva <bryanjsilva04@gmail.com>
2024-04-20 10:05:04 -0700

    Added a primary key to the dish-menu-station joint table

--- packages/db/src/schema/dish-menu-station-joint.ts
+++ packages/db/src/schema/dish-menu-station-joint.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { pgTable, text, primaryKey } from "drizzle-orm/pg-core";
 
 import { DishTable } from "./dish-table";
 import { MenuTable } from "./menu-table";
@@ -24,6 +24,10 @@ export const DishMenuStationJoint = pgTable("dish_menu_station_joint", {
       onDelete: "restrict",
       onUpdate: "cascade",
     }),
+}, (table) => {
+  return {
+    pk: primaryKey({ columns: [table.dishId, table.menuId, table.stationId] }),
+  };
 });
 
 export const dishMenuStationRelations = relations(
--- packages/db/src/schema/dish-table.ts
+++ packages/db/src/schema/dish-table.ts
@@ -10,7 +10,7 @@ import { updatedAtColumnPostgres } from "./utils";
 
 export const DishTable = pgTable("dishes", {
   id: text("id").primaryKey().notNull(),
-  name: text("name").notNull(),
+  name: text("name").notNull().unique(),
   description: text("description").notNull(),
   category: text("category").notNull(),
 
