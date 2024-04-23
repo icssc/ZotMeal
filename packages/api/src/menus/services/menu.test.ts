653df76cad8bc3b2c49c040e7952f5e6d63e3587
Katy Huang <katyhuang360166@gmail.com>
2024-04-22 20:14:12 -0700

    fix: fix db.transation bug to use trx instead of db

--- packages/api/src/menus/services/menu.test.ts
+++ packages/api/src/menus/services/menu.test.ts
@@ -15,100 +15,90 @@ describe("upsertMenu()", async () => {
   const { db } = await createDrizzle(
     "postgres://admin:admin@localhost:5434/zotmeal",
   );
-  it(
-    "inserts valid menu into db",
-    async () => {
-      // upsert dummy restaurant & period & menu -- then rollback. should pass if 'Rollback' is successfully thrown for each
-      const testMenus: Menu[] = [
-        {
-          id: "1",
-          restaurantId: "9999",
-          date: "01/17/2024",
-          period: "dinner",
-          start: "01/17/2024",
-          end: "01/17/2024",
-          price: "20.04",
-        },
-      ];
+  it("inserts valid menu into db", async () => {
+    // upsert dummy restaurant & period & menu -- then rollback. should pass if 'Rollback' is successfully thrown for each
+    const testMenus: Menu[] = [
+      {
+        id: "1",
+        restaurantId: "9999",
+        date: "01/17/2024",
+        period: "dinner",
+        start: "01/17/2024",
+        end: "01/17/2024",
+        price: "20.04",
+      },
+    ];
 
-      const testRestaurant: Restaurant = {
-        id: "9999",
-        name: "brandywine_test",
-      };
+    const testRestaurant: Restaurant = {
+      id: "9999",
+      name: "brandywine_test",
+    };
 
-      expect(testRestaurant).toBeTruthy();
+    expect(testRestaurant).toBeTruthy();
+    console.log("BEFORE TEST");
+    for (const testMenu of testMenus) {
+      await expect(async () => {
+        await db.transaction(async (trx) => {
+          console.log("HI");
+          // Insert a test restaurant
+          const restaurant = await upsertRestaurant(trx, testRestaurant);
+          console.log(restaurant);
+          expect(restaurant).toBeTruthy();
+          // Insert a test menu
+          const menu = await upsertMenu(trx, testMenu);
+          expect(menu).toBeTruthy();
+          // console.log("upsertedMenu:", menu);
 
-      for (const testMenu of testMenus) {
-        await expect(async () => {
-          await db.transaction(async (trx) => {
-            // Insert a test restaurant
-            const restaurant = await upsertRestaurant(db, testRestaurant);
-            expect(restaurant).toBeTruthy();
-            // Insert a test menu
-            const menu = await upsertMenu(trx, testMenu);
-            expect(menu).toBeTruthy();
-            // console.log("upsertedMenu:", menu);
+          trx.rollback();
+        });
+      }).rejects.toThrowError("Rollback");
+    }
+  });
+  it("updates existing menu in db", async () => {
+    const testMenus: Menu[] = [
+      {
+        id: "1",
+        restaurantId: "9999",
+        date: "01/17/2024",
+        period: "dinner",
+        start: "01/17/2024",
+        end: "01/17/2024",
+        price: "20.04",
+      },
+      {
+        // second menu with same id but different date
+        id: "1",
+        restaurantId: "9999",
+        date: "04/30/2024",
+        period: "dinner",
+        start: "01/17/2024",
+        end: "01/17/2024",
+        price: "20.04",
+      },
+    ];
 
-            trx.rollback();
-          });
-        }).rejects.toThrowError("Rollback");
-      }
-    },
-    {
-      timeout: 10000, // Timeout in milliseconds (10 seconds)
-    },
-  );
-  it(
-    "updates existing menu in db",
-    async () => {
-      const testMenus: Menu[] = [
-        {
-          id: "1",
-          restaurantId: "9999",
-          date: "01/17/2024",
-          period: "dinner",
-          start: "01/17/2024",
-          end: "01/17/2024",
-          price: "20.04",
-        },
-        {
-          // second menu with same id but different date
-          id: "1",
-          restaurantId: "9999",
-          date: "04/30/2024",
-          period: "dinner",
-          start: "01/17/2024",
-          end: "01/17/2024",
-          price: "20.04",
-        },
-      ];
+    const testRestaurant: Restaurant = {
+      id: "9999",
+      name: "brandywine_test",
+    };
 
-      const testRestaurant: Restaurant = {
-        id: "9999",
-        name: "brandywine_test",
-      };
+    // upsert dummy restaurant & period & menu. then rollback. should pass if 'Rollback' is the thrown error for each test
+    for (const testMenu of testMenus) {
+      await expect(async () => {
+        await db.transaction(async (trx) => {
+          // Insert a test restaurant
+          const restaurant = await upsertRestaurant(trx, testRestaurant);
+          expect(restaurant).toBeTruthy();
+          // Insert test menu
+          const menu = await upsertMenu(trx, testMenu);
+          expect(menu).toBeTruthy();
+          // console.log("upsertedMenu:", menu);
 
-      // upsert dummy restaurant & period & menu. then rollback. should pass if 'Rollback' is the thrown error for each test
-      for (const testMenu of testMenus) {
-        await expect(async () => {
-          await db.transaction(async (trx) => {
-            // Insert a test restaurant
-            const restaurant = await upsertRestaurant(db, testRestaurant);
-            expect(restaurant).toBeTruthy();
-            // Insert test menu
-            const menu = await upsertMenu(trx, testMenu);
-            expect(menu).toBeTruthy();
-            // console.log("upsertedMenu:", menu);
-
-            trx.rollback();
-          });
-        }).rejects.toThrowError("Rollback");
-      }
-    },
-    {
-      timeout: 10000, // Timeout in milliseconds (10 seconds)
-    },
-  );
+          trx.rollback();
+        });
+      }).rejects.toThrowError("Rollback");
+    }
+  });
 
   afterAll(async () => {
     // await db.$disconnect();
