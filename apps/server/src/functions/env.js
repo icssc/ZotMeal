a396985657005263bfa9016d3a9c36508030eb3e
Katy Huang <katyhuang360166@gmail.com>
2024-04-29 15:56:51 -0700

    only load env when in local

--- apps/server/src/functions/env.js
+++ apps/server/src/functions/env.js
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 import { z } from "zod";
 
-dotenv.config({
-  path: "../../../../.env",
-});
-
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({
+    path: "../../../../.env",
+  });
+}
 const envSchema = z.object({
   DATABASE_URL: z.string(),
-  AWS_ACCESS_KEY_ID: z.string(),
-  AWS_SECRET_ACCESS_KEY: z.string(),
 });
 const env = envSchema.parse(process.env);
+
 export { env };
