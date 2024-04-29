76f17b89d71748485f59d705630a6548d77d44b2
Katy Huang <katyhuang360166@gmail.com>
2024-04-29 14:07:12 -0700

    remove log

--- apps/server/src/functions/cron/updateDailyHandler.ts
+++ apps/server/src/functions/cron/updateDailyHandler.ts
@@ -14,7 +14,6 @@ import { env } from "../env";
 //   process.env.DATABASE_URL ?? "postgres://admin:admin@localhost:5434/zotmeal";
 
 const connectionString = env.DATABASE_URL;
-console.log(connectionString);
 export const main = async (_event, _context) => {
   try {
     const db = createDrizzle({ connectionString });
