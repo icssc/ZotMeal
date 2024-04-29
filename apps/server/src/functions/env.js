02664c008f9798cb6a2a2524ddfb162836ead8fc
Katy Huang <katyhuang360166@gmail.com>
2024-04-29 14:17:23 -0700

    change the env schema

--- apps/server/src/functions/env.js
+++ apps/server/src/functions/env.js
@@ -4,9 +4,11 @@ import { z } from "zod";
 dotenv.config({
   path: "../../../../.env",
 });
+
 const envSchema = z.object({
   DATABASE_URL: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
 });
-
 const env = envSchema.parse(process.env);
 export { env };
