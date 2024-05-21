f1514e8405c04db5d83aae934bdb63bf85d61cfd
Andrew Ho <andyminhtuanho@gmail.com>
2024-05-21 16:22:34 -0700

    some cards and stuffs

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout() {
                   },
                 }}
               >
+                <Stack.Screen name="(tabs)" />
                 {/* <Stack.Screen
                 name="events"
                 options={{
