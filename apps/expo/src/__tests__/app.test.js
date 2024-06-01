36c95207e1a86af11dde07a1149e3c2a77e71d54
Dennis Lustre <dennis.lustre@gmail.com>
2024-06-01 11:45:18 -0700

    move @types def to  devDeps, add jest testing deps

---
+++ apps/expo/src/__tests__/app.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Logo } from "~/components";
+
+describe("<App />", () => {
+  it("has 1 child", () => {
+    const tree = renderer.create(<Logo />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
+});
