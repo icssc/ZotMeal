474425d1ac1444ea6cd5d0a025b7899348672707
Jake Moy <apixelatedpug@gmail.com>
2025-04-23 15:15:04 -0700

    :sparkles: Wrap layout in tRPC provider

---
+++ apps/next/src/app/layout-client.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
+import { trpc } from '../utils/trpc';
+import superjson from "superjson";
+import Toolbar from "@/components/ui/toolbar";
+
+export function RootClient({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: 'http://localhost:3000/trpc',
+          transformer: superjson,
+          async headers() {
+            return {
+              "x-trpc-source": "next-js",
+            };
+          },
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Toolbar />
+        {children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
--- apps/next/src/app/layout.tsx
+++ apps/next/src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 import "./globals.css";
-import Toolbar from "@/components/ui/toolbar";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { trpc } from '../utils/trpc';
+import { getBaseUrl } from "../../../expo-rewrite/utils/api";
+import superjson from "superjson";
+import { RootClient } from './layout-client';
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-})
+});
 
 export const metadata: Metadata = {
   title: "ZotMeal",
@@ -15,16 +19,13 @@ export const metadata: Metadata = {
                 dining hall features and updates.`,
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <Toolbar></Toolbar>
-        {children}
+        <RootClient>
+          {children}
+        </RootClient>
       </body>
     </html>
   );
