a83def86c6e1cf7b700ef1b2935496de57f93236
EightBitByte <apixelatedpug@gmail.com>
2025-04-30 19:12:50 -0700

    :construction: Import errors :(

--- apps/next/src/components/ui/dishes-info.tsx
+++ apps/next/src/components/ui/dishes-info.tsx
@@ -7,6 +7,7 @@ import FoodCardSkeleton from "./food-card-skeleton"
 import MealDividerSkeleton from "./meal-divider-skeleton"
 import { trpc } from "@/utils/trpc"; // Import tRPC hook
 import { HallEnum, MealTimeEnum } from "@/utils/types";
+import { RestaurantInfo } from "@zotmeal/api";
 
 interface DishesInfoProps {
   hall: HallEnum,
@@ -22,8 +23,9 @@ export default function DishesInfo({hall, station, mealTime} : DishesInfoProps) 
     {staleTime: 2 * 60 * 60 * 1000} // 2 hour stale time
   );
 
-  let hallData = hall == HallEnum.ANTEATERY ? queryResponse?.anteatery.menus
-                              : queryResponse?.brandywine.menus;
+  let hallData = hall == HallEnum.ANTEATERY 
+              ? queryResponse?.anteatery
+              : queryResponse?.brandywine
 
   return (
     <div className="flex flex-col gap-6 mt-10 px-2 overflow-y-auto 
---
+++ apps/next/src/utils/funcs.ts
@@ -0,0 +1,5 @@
+function getStationsFromData(data) {
+    
+}
+
+export { getStationsFromData };
