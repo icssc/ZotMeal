0ae054f16c455fd8d041ae7c363b73c681fd1749
EightBitByte <apixelatedpug@gmail.com>
2025-05-20 18:21:23 -0700

    :lipstick: Add meal headers to dish info

--- apps/next/src/components/ui/dishes-info.tsx
+++ apps/next/src/components/ui/dishes-info.tsx
@@ -5,6 +5,7 @@ import FoodCard from "./card/food-card"
 import FoodCardSkeleton from "./skeleton/food-card-skeleton"
 import MealDividerSkeleton from "./skeleton/meal-divider-skeleton"
 import { DishInfo } from "@zotmeal/api";
+import MealDivider from "./meal-divider";
 
 interface DishesInfoProps {
   dishes: DishInfo[],
@@ -14,6 +15,15 @@ interface DishesInfoProps {
 }
 
 export default function DishesInfo({dishes, isLoading, isError, errorMessage} : DishesInfoProps) { 
+  // Sort the dishes by category string
+  let categoryMap: {[dishCategory : string]: DishInfo[]} = {};
+  dishes.forEach((dish) => {
+    if (dish.category in categoryMap)
+      categoryMap[dish.category].push(dish)
+    else
+      categoryMap[dish.category] = [dish]
+  })
+
   return (
     <div className="flex flex-col gap-6 mt-10 px-2 overflow-y-auto 
       flex-grow h-1" 
@@ -36,23 +46,20 @@ export default function DishesInfo({dishes, isLoading, isError, errorMessage} : 
 
       {!isLoading && !isError && (
         <> 
-          {(() => {
-            if (!dishes) {
-              return (
-                <p className="text-center text-gray-500">
-                  Dining hall data not found for the selected hall.
-                </p>
-              );
-            }
-
-            return (
-              <>
-                {dishes.map(dish => (
-                  <FoodCard key={dish.id} {... dish}/>
-                ))}
-              </>
-            );
-          })()}
+          {dishes.length === 0 ? (
+            <p className="text-center text-gray-500 py-4">
+              No dishes available for this selection.
+            </p>
+          ) : (
+                Object.keys(categoryMap).map(categoryString => 
+                  <>
+                    <MealDivider title={categoryString} key={`${categoryString}+${categoryMap[categoryString][0].stationId}`}/>
+                    {categoryMap[categoryString].map(dish => 
+                      <FoodCard key={dish.id} {... dish}/>
+                    )}
+                  </>
+                )
+            )}
         </>
       )}
 
