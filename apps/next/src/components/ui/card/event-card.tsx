df75111f1ba4873921685c25714a26f967e3d968
Lex Truong <lext@uci.edu>
2025-06-02 03:09:04 -0400

    event card content does not overflow

--- apps/next/src/components/ui/card/event-card.tsx
+++ apps/next/src/components/ui/card/event-card.tsx
@@ -35,24 +35,28 @@ const EventCardContent = React.forwardRef<
     return (
       <div ref={ref} {...divProps}>
         <Card className="cursor-pointer hover:shadow-lg transition">
-          <CardContent className="flex items-center h-full pt-6 gap-6">
+          <CardContent className="flex items-center h-full w-full pt-6 gap-6 flex-wrap">
             <Image 
               src={props.imgSrc}
               alt={props.alt}
               width={200}
               height={110}
-              className="rounded-sm"
+              className="w-[200px] h-[110px]rounded-sm"
             />
             <div className="flex flex-col gap-1 h-full" id="event-card-content">
             <div className="flex flex-row gap-2">
               <strong className="text-2xl">{props.name}</strong>
               {props.isOngoing && <OngoingBadge/>}
             </div>
-            <div className="text-zinc-400 flex gap-1" id="event-card-subheader">
+            <div className="text-zinc-400 flex flex-col sm:flex-row gap-x-4 gap-y-1" id="event-card-subheader">
+              <div className="flex gap-1">
                 <Clock className="stroke-zinc-400"/>
                 <p>{dateToString(props.startTime, props.endTime)}</p>
+              </div>
+              <div className="flex gap-1">
                 <MapPinned/>
                 <p>{toTitleCase(HallEnum[props.location])}</p>
+              </div>
             </div>
             <p className="max-w-xl">{props.shortDesc}</p>
             </div>
