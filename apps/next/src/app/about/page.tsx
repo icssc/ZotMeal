ca1b5b55fb0c1064e54fd1fb410032f8394fee25
Lex Truong <lext@uci.edu>
2025-06-17 17:04:35 -0700

    privacy policy drawer on mobile

--- apps/next/src/app/about/page.tsx
+++ apps/next/src/app/about/page.tsx
@@ -9,11 +9,73 @@ import { Dialog,
   DialogTitle,
   DialogTrigger,
  } from "@/components/ui/shadcn/dialog";
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/shadcn/drawer";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { trpc } from "@/utils/trpc";
 import Image from "next/image";
 
+const PrivacyDescription = () => {
+   return (
+    <div>
+      <p className="mb-4">
+        ZotMeal is a cross-platform mobile application designed to help users view 
+        dining hall menus at the University of California, Irvine (UCI). We value 
+        your privacy and are committed to protecting any personal information you 
+        may share with us.
+      </p>
+      <p className="mb-4">
+        ZotMeal does not collect or store any personally identifiable information. 
+        The app does not require login or account creation. We do not track 
+        or monitor user behavior within the app.
+      </p>
+      <p className="mb-4">
+        ZotMeal fetches dining hall menu data from publicly available or authorized 
+        UCI resources. This data is used solely to display daily menus within the app 
+        and is not shared or stored beyond your device.
+      </p>
+    </div>
+  );
+};
+
+const PrivacyDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="w-fit">Privacy Policy</Button>
+      </DialogTrigger>
+      <DialogContent className="w-md h-auto">
+        <DialogHeader>
+          <DialogTitle className="p-4 text-center">Privacy Policy</DialogTitle>
+          <DialogDescription asChild className="px-4">
+            {PrivacyDescription()}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const PrivacyDrawer = () => {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button className="w-fit">Privacy Policy</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle className="p-4 text-center">Privacy Policy</DrawerTitle>
+          <DrawerDescription asChild className="px-12">
+            {PrivacyDescription()}
+          </DrawerDescription>
+        </DrawerHeader>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
 export default function About() {
   const {data: queryResponse, isLoading, isError, error } = trpc.zotmeal_contributors.useQuery();
+  const isDesktop = useMediaQuery('(min-width: 768px)');
 
   return (
     <div className="flex flex-col">
@@ -62,38 +124,20 @@ export default function About() {
                 Discord
                 </a>!
             </p>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button className="w-fit">Privacy Policy</Button>
-              </DialogTrigger>
-              <DialogContent className="w-md h-auto">
-                <DialogHeader>
-                  <DialogTitle className="p-4 text-center">Privacy Policy</DialogTitle>
-                  <DialogDescription asChild className="p-4">
-                    <div>
-                      <p className="mb-4">
-                        ZotMeal is a cross-platform mobile application designed to help users view 
-                        dining hall menus at the University of California, Irvine (UCI). We value 
-                        your privacy and are committed to protecting any personal information you 
-                        may share with us.
-                      </p>
-                      <p className="mb-4">
-                        ZotMeal does not collect or store any personally identifiable information. 
-                        The app does not require login or account creation. We do not track 
-                        or monitor user behavior within the app.
-                      </p>
-                      <p className="mb-4">
-                        ZotMeal fetches dining hall menu data from publicly available or authorized 
-                        UCI resources. This data is used solely to display daily menus within the app 
-                        and is not shared or stored beyond your device.
-                      </p>
-                    </div>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+
+          {isDesktop ? (
+            <div>
+              {PrivacyDialog()}
+            </div>
+          ) : (
+            <div className="flex justify-center">
+              {PrivacyDrawer()}
+            </div>
+          )}
+
           </div>
         </div>
+
         <div className="flex flex-col items-center gap-4" id="contributors">
           <h1 className="text-xl max-md:text-base max-sm:text-sm font-bold">Our Lovely Contributors</h1>
           <div className="flex flex-wrap justify-center gap-2 max-w-xs" id="contributor-grid">
