063ef3dc242c5f6ee63059a0c99cea518f7cfb73
Lex Truong <lext@uci.edu>
2025-11-08 14:52:07 -0800

    fix: skip scrapeEvents test

--- packages/api/src/server/scrapeEvents/scrapeEvents.test.ts
+++ packages/api/src/server/scrapeEvents/scrapeEvents.test.ts
@@ -1,29 +1,29 @@
 // Deprecated.
 // Get list of events from GraphQL Endpoint AEM_EventList()
 
-// import { apiTest } from "@api/apiTest";
-// import { upsertEvents } from "@api/events/services";
-// import { describe } from "vitest";
+import { apiTest } from "@api/apiTest";
+import { upsertEvents } from "@api/events/services";
+import { describe } from "vitest";
 
-// import { getHTML, scrapeEvents } from ".";
+import { getHTML, scrapeEvents } from ".";
 
-// describe("scrapeEvents", () => {
-//   apiTest(
-//     "scrapes events data and inserts into db",
-//     async ({ expect, db }) => {
-//       await expect(
-//         db.transaction(async (trx) => {
-//           const html = await getHTML(
-//             "https://uci-campusdish-com.translate.goog/api/events?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
-//           );
-//           const events = await scrapeEvents(html);
-//           expect(events).toBeTruthy();
+describe("scrapeEvents", () => {
+  apiTest.skip(
+    "scrapes events data and inserts into db",
+    async ({ expect, db }) => {
+      await expect(
+        db.transaction(async (trx) => {
+          const html = await getHTML(
+            "https://uci-campusdish-com.translate.goog/api/events?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+          );
+          const events = await scrapeEvents(html);
+          expect(events).toBeTruthy();
 
-//           await expect(upsertEvents(trx, events)).resolves.toBeDefined();
-//           trx.rollback();
-//         }),
-//       ).rejects.toThrowError("Rollback");
-//     },
-//     30_000,
-//   );
-// }, 30_000);
+          await expect(upsertEvents(trx, events)).resolves.toBeDefined();
+          trx.rollback();
+        }),
+      ).rejects.toThrowError("Rollback");
+    },
+    30_000,
+  );
+}, 30_000);
