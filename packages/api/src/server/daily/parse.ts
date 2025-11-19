ec52d0e5acf567c0ff2a6ca31aa71dbfb939d6bc
MihaiZecheru <zecheruchris@gmail.com>
2025-11-19 13:42:27 -0800

    fix(test:daily): file path formatting

--- packages/api/src/server/daily/parse.ts
+++ packages/api/src/server/daily/parse.ts
@@ -72,7 +72,7 @@ export async function queryAdobeECommerce(
     };
 
     if (process.env.IS_LOCAL) {
-      const outPath = `query-${new Date().toISOString()}-response.json`
+      const outPath = `query-${new Date().toISOString().replace(/:/g, '-')}-response.json`;
       writeFileSync(`./${outPath}`, JSON.stringify(loggedResponse), { flag: "w" });
       logger.info(`[query] Wrote AdobeEcommerce response to ${process.cwd()}/${outPath}.`);
     }
