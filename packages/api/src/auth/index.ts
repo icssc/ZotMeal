503c504b8f614b0162e69273795f97f974339746
Pranav Batra <30417498+Pranav-Batra@users.noreply.github.com>
2025-11-21 17:31:38 -0800

    Update index.ts - remove secrets

--- packages/api/src/auth/index.ts
+++ packages/api/src/auth/index.ts
@@ -1,34 +1,4 @@
-/* 
-FOR GOOGLE AUTH -> SETS UP GOOGLE AUTH PROVIDER FROM BETTERAUTH
-*/
-// import { betterAuth } from "better-auth";
 
-
-// export const auth = betterAuth({
-//   secret: process.env.BETTER_AUTH_SECRET!,
-//   baseURL: process.env.BASE_URL || "http://localhost:3000",
-//   socialProviders: {
-//     google: {
-//         clientId: process.env.GOOGLE_CLIENT_ID!,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     },
-//   }
-// })
-
-
-// import { betterAuth } from "better-auth";
-
-
-// export const auth = betterAuth({
-//   secret: process.env.BETTER_AUTH_SECRET || "better-auth-secret-123456789-xxxx",
-//   baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
-//   socialProviders: {
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID || "55792718512-74qt5ig2kkjkb01tep06c84rmscepoab.apps.googleusercontent.com",
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
-//     },
-//   },
-// });
 
 import { betterAuth } from "better-auth";
 
