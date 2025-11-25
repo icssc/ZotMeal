c0d8788ea6399e28d07f5db74637f928ff305910
Jason Lin <jkhlin@uci.edu>
2025-11-24 22:19:08 -0800

    updated packages

--- apps/next/src/components/auth/google-sign-in.tsx
+++ apps/next/src/components/auth/google-sign-in.tsx
@@ -9,8 +9,8 @@ import { Button } from "../ui/shadcn/button";
 export function GoogleSignInButton() {
   const handleSignIn = () => {
         authClient.signIn.social({
-            provider: "google",
-            callbackURL: '/'
+            provider: "google", // starts a Google sign in flow
+            callbackURL: '/' // after a successful sign-in, user is redirected to the home page
         }
     );
 }
