233bc22ff6d516bafd421f0cbb2c469e7189fa53
Pranav Batra <30417498+Pranav-Batra@users.noreply.github.com>
2025-12-01 16:14:06 -0800

    chore(auth): removed sensitive console.log statements

--- packages/api/src/auth/index.ts
+++ packages/api/src/auth/index.ts
@@ -14,7 +14,7 @@ config({ path: join(process.cwd(), ".env") });
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+// console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 export const auth = betterAuth({
   debug: true,
